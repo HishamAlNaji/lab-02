d882ee8dd3b146607065867b20ded6c7398d44ce
HishamAlNaji <hishamnaji1010@gmail.com>
2020-07-21 22:16:27 +0300

    lab 03 ready

--- js/app.js
+++ js/app.js
@@ -1,10 +1,33 @@
 'use strict'
 
 var animals = [];
-
 var keywordsArr = [];
 
-// animal objects constructor
+// getting the data from json files :
+
+function pageOne() {
+    $.get('data/page-1.json').then(data => {
+        data.forEach(animal => {
+            let newAnimal = new Animal(animal.image_url, animal.title, animal.description, animal.keyword, animal.horns)
+            newAnimal.render();
+            newAnimal.options();
+
+        });
+    });
+}
+
+function pageTwo() {
+    $.get('data/page-2.json').then(data => {
+        data.forEach(animal => {
+            let newAnimal = new Animal(animal.image_url, animal.title, animal.description, animal.keyword, animal.horns)
+            newAnimal.render();
+            newAnimal.options();
+        });
+
+    });
+}
+
+// animal objects constructor :
 
 function Animal(img, title, description, keyword, horns) {
     this.image_url = img;
@@ -15,46 +38,83 @@ function Animal(img, title, description, keyword, horns) {
     animals.push(this);
 }
 
-// getting the data from json file 
+pageOne();
 
-$.get('data/page-1.json').then(data => {
-    data.forEach(animal => {
-        let newAnimal = new Animal(animal.image_url, animal.title, animal.description, animal.keyword, animal.horns)
-        newAnimal.render();
-        newAnimal.options();
-    });
+// event for page one :
 
-});
+$('#page1').on('click', function() {
+    $('.page').remove();
+    animals = [];
+    pageOne();
+})
+
+$('#page2').on('click', function() {
+    $('.page').remove();
+    animals = [];
+    pageTwo();
+})
 
 renderSelected();
+renderSorted()
 
 // Render the photos for animals 
 
 Animal.prototype.render = function() {
-    let photoTemplate = $('#photo-template');
-    photoTemplate = $('#photo-template').clone();
-    photoTemplate.removeAttr('id');
-    photoTemplate.attr('class', this.keyword);
-    photoTemplate.find('h2').text(this.title);
-    photoTemplate.find('img').attr('src', this.image_url);
-    photoTemplate.find('p').text(this.description);
-    $('main').append(photoTemplate);
+    // let photoTemplate = $('#photo-template');
+    // photoTemplate = $('#photo-template').clone();
+    // photoTemplate.removeAttr('id');
+    // photoTemplate.attr('class', this.keyword);
+    // photoTemplate.addClass('page');
+    // photoTemplate.find('h2').text(this.title);
+    // photoTemplate.find('img').attr('src', this.image_url);
+    // photoTemplate.find('p').text(this.description);
+    // $('main').append(photoTemplate);
+    let mustacheTemplate = $('#mustache-template').html();
+    let htmlPage = Mustache.render(mustacheTemplate, this);
+    $('main').append(htmlPage);
+    $('section').addClass('page');
+
     // console.log(this);
 }
 
 Animal.prototype.options = function() {
     if (keywordsArr.includes(this.keyword) === false) {
-        $('select').append(`<option>${this.keyword}</option>`);
+        $('#filter').append(`<option>${this.keyword}</option>`);
         keywordsArr.push(this.keyword);
     }
 };
 
 function renderSelected() {
-
-    $('select').change(function() {
-        $('section').hide();
-        var value = $(this).val();
-        console.log(value);
-        $(`.${value}`).show();
+    $('#filter').change(function() {
+        $('.page').remove();
+        for (let i = 0; i < animals.length; i++) {
+            if (animals[i].keyword === $(this).val()) {
+                animals[i].render();
+            }
+        }
     });
 };
+
+function renderSorted() {
+    $('#sort').change(function() {
+
+        $('.page').remove();
+
+        if ('title' === $(this).val()) {
+            animals.sort((a, b) => {
+                return a.title.localeCompare(b.title)
+            });
+            for (let i = 0; i < animals.length; i++) {
+                animals[i].render();
+            }
+        } else if ('horns' === $(this).val()) {
+            animals.sort((a, b) => {
+                return a.horns - b.horns
+            });
+            for (let i = 0; i < animals.length; i++) {
+                animals[i].render();
+            }
+        }
+
+    });
+}
